53e166d2baa112018c05a4db34b90cfd1819975c
AntPzk90 <44503472+AntPzk90@users.noreply.github.com>
2021-09-02 12:11:42 +0300

    add data from fetch

--- js/main.js
+++ js/main.js
@@ -1,13 +1,23 @@
-import {data} from './mocks.js'
 import {setFiltersListener} from './filter.js'
 import {getPins} from './pins.js'
 
+  
+fetch('http://www.academy-booking-api.staj.fun/')
+    
+  .then((response) => {
+      console.log("ФЕТЧ ОТРАБОТАЛ")
+      console.log(response)
+    return response.json();
+  })
+  .then((data) => {
 
+    let filteredData = data
 
+    setFiltersListener(data, filteredData)
 
+    getPins(filteredData);
 
-let filteredData = data
-
-setFiltersListener(data, filteredData)
-
-getPins(filteredData)
+  }).catch((e) => {
+    console.log(e, "error")
+    throw new Error(e);
+})
